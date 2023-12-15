1cd96074a85763c02319e84b3f0e5bc141e3d734
agupta800 <awadheshg577@gmail.com>
2023-12-15 11:50:56 +0545

    project added

---
+++ Project1/Models/FormModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Project1.Models
+{
+    public class FormModel
+    {
+        public string mantralaya { get; set; }
+
+        public string formNumber { get; set; }
+
+        public int demandFormNumber { get; set; }
+
+
+        public DateTime datepicker { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
+    
+
